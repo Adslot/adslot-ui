51c5347fbb92a2559fcf9aeeefd448eb0fc8190d
Cameron Chamberlain <git@camjc.com>
2015-12-18 09:38:32 +1100

    Import Alexandria
    - Add note to Readme about how to profile the webpack dist.
    - Finally decide that react-bootstrap is a dev dependency and we
      should ship the parts we're exporting. This way we can load only the
      modules we use in Direct and Symphony etc.
    - Update package.json to point to public npm and our git repo.

--- src/components/distributionEntry.js
+++ src/components/distributionEntry.js
@@ -6,10 +6,28 @@ import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
+import {
+  Alert,
+  Breadcrumb,
+  Empty,
+  Grid,
+  GridCell,
+  GridRow,
+  Search,
+  Slicey,
+} from 'alexandria-adslot';
 
 module.exports = {
+  Alert,
+  Breadcrumb,
   Button,
+  Empty,
+  Grid,
+  GridCell,
+  GridRow,
   Modal,
   Nav,
   NavItem,
+  Search,
+  Slicey,
 };
