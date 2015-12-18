e486e74bdf057a4dd2002423512599e0778b6f28
Rob Munro <robmunro@gmail.com>
2015-12-18 12:17:03 +1100

    Added custom checkbox and radiobutton

--- test/components/MainTest.js
+++ test/components/MainTest.js
@@ -3,6 +3,12 @@
 
 import createComponent from 'helpers/shallowRenderHelper';
 import Main from 'components/Main';
+import { isElementOfType } from 'react-addons-test-utils';
+import {
+  Checkbox,
+  Radio,
+  RadioGroup,
+} from '../../src/components/distributionEntry';
 
 describe('MainComponent', () => {
   let MainComponent;
@@ -21,5 +27,21 @@ describe('MainComponent', () => {
     expect(modalComponent.props.bsSize).to.equal('small');
     expect(modalComponent.props.keyboard).to.equal(false);
     expect(modalComponent.props.backdrop).to.equal(true);
+  });
+
+  it('should have a checkbox component', () => {
+    const checkboxExampleContainer = MainComponent.props.children[10];
+    const checkboxComponent = checkboxExampleContainer.props.children;
+    expect(isElementOfType(checkboxComponent, Checkbox)).to.equal(true);
+    expect(checkboxComponent.props.label).to.equal('Unchecked');
+  });
+
+  it('should have a radio button component', () => {
+    const radioButtonContainer = MainComponent.props.children[15];
+    const radioGroupComponent = radioButtonContainer.props.children;
+    expect(isElementOfType(radioGroupComponent, RadioGroup)).to.equal(true);
+    const radioComponent = radioGroupComponent.props.children[0];
+    expect(isElementOfType(radioComponent, Radio)).to.equal(true);
+    expect(radioComponent.props.label).to.equal('Unchecked');
   });
 });
