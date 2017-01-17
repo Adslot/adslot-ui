470bb07df17cf8f7bf9568f1cefbdefdb246a316
Vincent Teo <vincent.teo@adslot.com>
2017-01-17 11:14:46 +1100

    New: Ability to group nodes in TreePickerGrid component

--- src/components/adslotUi/TreePickerGridComponent.jsx
+++ src/components/adslotUi/TreePickerGridComponent.jsx
@@ -3,14 +3,17 @@ import fastStatelessWrapper from 'components/adslotUi/fastStatelessWrapper';
 import TreePickerNode from 'components/adslotUi/TreePickerNodeComponent';
 import TreePickerPropTypes from 'helpers/propTypes/TreePickerPropTypes';
 import React, { PropTypes } from 'react';
-import { Empty, Grid, SvgSymbol } from 'alexandria-adslot';
+import { Empty, Grid, GridRow, SvgSymbol } from 'alexandria-adslot';
 
-const TreePickerNodeFast = fastStatelessWrapper(TreePickerNode, ['node.id', 'disabled', 'selected']);
+require('styles/adslotUi/TreePickerGrid.scss');
+
+export const TreePickerNodeFast = fastStatelessWrapper(TreePickerNode, ['node.id', 'disabled', 'selected']);
 
 const TreePickerGridComponent = ({
   disabled,
   emptySvgSymbol,
   expandNode,
+  groupFormatter,
   hideIcon,
   includeNode,
   itemType,
@@ -20,34 +23,48 @@ const TreePickerGridComponent = ({
   selected,
   valueFormatter,
   emptyText,
-}) => (
-  <Grid>
-    {_.map(nodes, (node) =>
-      <TreePickerNodeFast
-        key={node.id}
-        {...{
-          disabled,
-          expandNode,
-          includeNode,
-          itemType,
-          node,
-          nodeRenderer,
-          removeNode,
-          selected,
-          valueFormatter,
-        }}
-      />
-    )}
-    {nodes ?
-      <Empty
-        collection={nodes}
-        hideIcon={hideIcon}
-        svgSymbol={emptySvgSymbol}
-        text={emptyText}
-      /> :
-      null}
-  </Grid>
-);
+}) => {
+  const nodesByGroupLabel = _.groupBy(nodes, groupFormatter);
+  return (
+    <Grid>
+      {_.map(nodesByGroupLabel, (groupedNodes, label) =>
+        <div className="treepickergrid-component-group" key={_.kebabCase(label)}>
+          {_.size(nodesByGroupLabel) > 1 ?
+            <div className="treepickergrid-component-group-label">
+              <GridRow dts={`group-label-${_.kebabCase(label)}`}>
+                {label}
+              </GridRow>
+            </div> :
+            null}
+          {_.map(groupedNodes, (node) =>
+            <TreePickerNodeFast
+              key={node.id}
+              {...{
+                disabled,
+                expandNode,
+                includeNode,
+                itemType,
+                node,
+                nodeRenderer,
+                removeNode,
+                selected,
+                valueFormatter,
+              }}
+            />
+          )}
+        </div>
+      )}
+      {nodes ?
+        <Empty
+          collection={nodes}
+          hideIcon={hideIcon}
+          svgSymbol={emptySvgSymbol}
+          text={emptyText}
+        /> :
+        null}
+    </Grid>
+  );
+};
 
 TreePickerGridComponent.displayName = 'AdslotUiTreePickerGridComponent';
 
@@ -56,6 +73,7 @@ TreePickerGridComponent.propTypes = {
   emptySvgSymbol: PropTypes.shape(SvgSymbol.propTypes),
   emptyText: PropTypes.any.isRequired,
   expandNode: PropTypes.func,
+  groupFormatter: PropTypes.func,
   hideIcon: PropTypes.bool,
   includeNode: PropTypes.func,
   itemType: PropTypes.string.isRequired,
@@ -69,6 +87,7 @@ TreePickerGridComponent.propTypes = {
 TreePickerGridComponent.defaultProps = {
   disabled: false,
   hideIcon: false,
+  groupFormatter: () => 'Default Group',
 };
 
 export default TreePickerGridComponent;
--- test/components/adslotUi/TreePickerGridComponentTest.jsx
+++ test/components/adslotUi/TreePickerGridComponentTest.jsx
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
-import TreePickerGrid from 'components/adslotUi/TreePickerGridComponent';
+import TreePickerGrid, { TreePickerNodeFast } from 'components/adslotUi/TreePickerGridComponent';
 import TreePickerMocks from 'mocks/TreePickerMocks';
 import { Empty, Grid } from 'alexandria-adslot';
 import { shallow } from 'enzyme';
@@ -31,10 +31,55 @@ describe('TreePickerGridComponent', () => {
     const component = shallow(<TreePickerGrid {...props} />);
     const gridElement = component.find(Grid);
     expect(gridElement).to.have.length(1);
+    expect(gridElement.children()).to.have.length(2); // Group node and an Empty.
 
-    expect(gridElement.children()).to.have.length(3); // Two nodes and an Empty.
+    const groupElement = gridElement.find('.treepickergrid-component-group');
+    expect(groupElement).to.have.length(1);
+    expect(groupElement.children()).to.have.length(2); // Two nodes
+
     _.forEach(props.nodes, (node, index) => {
-      const nodeElement = gridElement.childAt(index);
+      const nodeElement = groupElement.childAt(index);
+      expect(nodeElement.prop('expandNode')).to.equal(props.expandNode);
+      expect(nodeElement.prop('includeNode')).to.equal(props.includeNode);
+      expect(nodeElement.prop('removeNode')).to.equal(props.removeNode);
+      expect(nodeElement.prop('node')).to.equal(node);
+    });
+
+    const emptyElement = gridElement.find(Empty);
+    expect(emptyElement).to.have.length(1);
+    expect(emptyElement.prop('collection')).to.equal(props.nodes);
+    expect(emptyElement.prop('svgSymbol')).to.equal(props.emptySvgSymbol);
+    expect(emptyElement.prop('text')).to.equal(props.emptyText);
+  });
+
+  it('should render with groups', () => {
+    const props = {
+      emptySvgSymbol: svgSymbol,
+      emptyText: 'Empty!',
+      expandNode: _.noop,
+      groupFormatter: (node) => node.id,
+      includeNode: _.noop,
+      itemType,
+      nodes: [qldNode, saNode],
+      nodeRenderer,
+      removeNode: _.noop,
+      selected: false,
+      valueFormatter,
+    };
+    const component = shallow(<TreePickerGrid {...props} />);
+    const gridElement = component.find(Grid);
+    expect(gridElement).to.have.length(1);
+    expect(gridElement.children()).to.have.length(3); // Two group node and an Empty.
+
+    const groupElements = gridElement.find('.treepickergrid-component-group');
+    expect(groupElements).to.have.length(2);
+
+    _.forEach(props.nodes, (node, index) => {
+      const groupElement = groupElements.at(index);
+      const labelElement = groupElement.find('.treepickergrid-component-group-label');
+      expect(labelElement.children().children().text()).to.equal(node.id);
+
+      const nodeElement = groupElement.find(TreePickerNodeFast);
       expect(nodeElement.prop('expandNode')).to.equal(props.expandNode);
       expect(nodeElement.prop('includeNode')).to.equal(props.includeNode);
       expect(nodeElement.prop('removeNode')).to.equal(props.removeNode);
