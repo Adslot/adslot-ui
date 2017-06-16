135e9d5bcaf949f46610a77c88ab948065c16b56
Patrick Ivers <patrick.ivers@adslot.com>
2017-06-16 11:11:25 +1000

    Fix: AlertInput focus
    - Add focus border style to alert input
    - Select all input content on focus (instead of on click)

--- src/components/adslotUi/AlertInput/index.spec.jsx
+++ src/components/adslotUi/AlertInput/index.spec.jsx
@@ -2,94 +2,155 @@
 import _ from 'lodash';
 import React from 'react';
 import sinon from 'sinon';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import Overlay from 'react-bootstrap/lib/Overlay';
 import AlertInput from '.';
 
 describe('AlertInput', () => {
-  it('should render with input props', () => {
-    const props = {
-      value: 'lorem',
-      onValueChange: _.noop,
-      onBlur: _.noop,
-    };
-    const component = shallow(<AlertInput {...props} />);
-    expect(component.prop('trigger')).to.eql(['hover', 'focus']);
-    expect(component.prop('placement')).to.equal('bottom');
-    expect(component.prop('overlay').type).to.equal('div');
-    expect(component.prop('overlay').className).to.be.an('undefined');
+  const initialState = {
+    isFocused: false,
+    isPopoverVisible: false,
+  };
 
-    const rootElement = component.childAt(0);
-    expect(rootElement.hasClass('alert-input-component')).to.equal(true);
-    expect(rootElement.children()).to.have.length(1);
+  describe('handleMouseEnter()', () => {
+    it('should set `isPopoverVisible` to true if alert message exists', () => {
+      const component = shallow(<AlertInput alertMessage="Hello" />);
+      component.simulate('mouseenter');
+      expect(component.state()).to.eql({
+        isFocused: false,
+        isPopoverVisible: true,
+      });
+    });
 
-    const inputElement = rootElement.childAt(0);
-    expect(inputElement.prop('className')).to.equal('alert-input-component-input');
-    expect(inputElement.prop('type')).to.equal('text');
-    expect(inputElement.prop('value')).to.equal('lorem');
-    expect(inputElement.prop('onClick')).to.be.a('function');
-    expect(inputElement.prop('onChange')).to.be.a('function');
-    expect(inputElement.prop('onBlur')).to.be.a('function');
+    it('should not set `isPopoverVisible` to true if no alert message exists', () => {
+      const component = shallow(<AlertInput />);
+      component.simulate('mouseenter');
+      expect(component.state()).to.eql(initialState);
+    });
   });
 
-  it('should render with addons', () => {
-    const props = {
-      prefixAddon: '$',
-      suffixAddon: '.00',
-    };
-    const component = shallow(<AlertInput {...props} />);
-    const rootElement = component.childAt(0);
-    expect(rootElement.children()).to.have.length(3);
-
-    const prefixElement = rootElement.childAt(0);
-    expect(prefixElement.text()).to.equal('$');
-
-    const suffixElement = rootElement.childAt(2);
-    expect(suffixElement.text()).to.equal('.00');
+  describe('handleMouseLeave()', () => {
+    it('should set `isPopoverVisible` to false', () => {
+      const component = shallow(<AlertInput alertMessage="Hello" />);
+      component.simulate('mouseenter');
+      component.simulate('mouseleave');
+      expect(component.state()).to.eql(initialState);
+    });
   });
 
-  it('should render with alert status', () => {
-    const props = {
-      alertStatus: 'error',
-    };
-    const component = shallow(<AlertInput {...props} />);
-    const rootElement = component.childAt(0);
-    expect(rootElement.prop('className')).to.equal('alert-input-component error');
-    expect(rootElement.children()).to.have.length(1);
+  describe('handleInputFocus()', () => {
+    it('should set `isFocused` to true, and `isPopoverVisible` if there is an alert message', () => {
+      const component = shallow(<AlertInput alertMessage="Hello" />);
+      const inputElement = component.find('.alert-input-component-input');
+      const focusEvent = {
+        target: {
+          select: sinon.spy(),
+        },
+      };
+      inputElement.simulate('focus', focusEvent);
+      expect(component.state()).to.eql({
+        isFocused: true,
+        isPopoverVisible: true,
+      });
+      expect(focusEvent.target.select.callCount).to.equal(1);
+    });
+
+    it('should set `isFocused` to true, but not `isPopoverVisible` if no alert message', () => {
+      const component = shallow(<AlertInput />);
+      const inputElement = component.find('.alert-input-component-input');
+      const focusEvent = {
+        target: {
+          select: sinon.spy(),
+        },
+      };
+      inputElement.simulate('focus', focusEvent);
+      expect(component.state()).to.eql({
+        isFocused: true,
+        isPopoverVisible: false,
+      });
+      expect(focusEvent.target.select.callCount).to.equal(1);
+    });
   });
 
-  it('should render Popover when there is an alert message', () => {
-    const props = {
-      alertStatus: 'error',
-      alertMessage: 'Lorem ipsum dolor sit amet.',
-    };
-    const component = shallow(<AlertInput {...props} />);
-    const popover = component.prop('overlay');
-    expect(popover.props.className).to.equal('alert-input-component-popover error');
-    expect(popover.props.id).to.equal('alert-input-popover');
+  describe('handleInputBlur ()', () => {
+    it('should set `isFocused` and `isPopoverVisible` to false', () => {
+      const component = shallow(<AlertInput />);
+      const inputElement = component.find('.alert-input-component-input');
+      const focusEvent = {
+        target: {
+          select: _.noop,
+        },
+      };
+      inputElement.simulate('focus', focusEvent);
+      inputElement.simulate('blur');
+      expect(component.state()).to.eql(initialState);
+    });
+
+    it('should call `onBlur` if exists', () => {
+      const onBlurSpy = sinon.spy();
+      const component = shallow(<AlertInput onBlur={onBlurSpy} />);
+      const inputElement = component.find('.alert-input-component-input');
+      inputElement.simulate('blur');
+      expect(onBlurSpy.callCount).to.equal(1);
+    });
   });
 
-  it('should handle input change event', () => {
-    const props = {
-      value: 'abc',
-      onValueChange: sinon.spy(),
-    };
-    const component = shallow(<AlertInput {...props} />);
-    const inputElement = component.find('.alert-input-component-input');
-    inputElement.simulate('change', 'xyz');
-    expect(props.onValueChange.callCount).to.equal(1);
-    expect(props.onValueChange.args[0][0]).to.equal('xyz');
-  });
+  describe('render()', () => {
+    it('should render with input props', () => {
+      const props = {
+        value: 'lorem',
+        onValueChange: _.noop,
+        onBlur: _.noop,
+      };
+      const component = shallow(<AlertInput {...props} />);
+      expect(component.hasClass('alert-input-component')).to.equal(true);
+      expect(component.prop('onMouseEnter')).to.be.a('function');
+      expect(component.prop('onMouseLeave')).to.be.a('function');
+      expect(component.children()).to.have.length(2);
 
-  it('should call selectAll() on input click', () => {
-    const component = shallow(<AlertInput />);
-    const inputElement = component.find('.alert-input-component-input');
-    const onInputClick = inputElement.prop('onClick');
-    const eventSpy = {
-      target: {
-        select: sinon.spy(),
-      },
-    };
-    onInputClick(eventSpy);
-    expect(eventSpy.target.select.callCount).to.equal(1);
+      const inputElement = component.childAt(0);
+      expect(inputElement.prop('className')).to.equal('alert-input-component-input');
+      expect(inputElement.prop('type')).to.equal('text');
+      expect(inputElement.prop('value')).to.equal('lorem');
+      expect(inputElement.prop('onChange')).to.be.a('function');
+      expect(inputElement.prop('onFocus')).to.be.a('function');
+      expect(inputElement.prop('onBlur')).to.be.a('function');
+
+      const overlayElement = component.childAt(1);
+      expect(overlayElement.prop('show')).to.equal(false);
+      expect(overlayElement.prop('target')).to.be.a('function');
+      expect(overlayElement.prop('placement')).to.equal('bottom');
+    });
+
+    it('should render with addons', () => {
+      const props = {
+        prefixAddon: '$',
+        suffixAddon: '.00',
+      };
+      const component = shallow(<AlertInput {...props} />);
+      expect(component.children()).to.have.length(4);
+
+      const prefixElement = component.childAt(0);
+      expect(prefixElement.text()).to.equal('$');
+
+      const suffixElement = component.childAt(2);
+      expect(suffixElement.text()).to.equal('.00');
+    });
+
+    it('should render with alert status', () => {
+      const props = {
+        alertStatus: 'error',
+      };
+      const component = shallow(<AlertInput {...props} />);
+      expect(component.prop('className')).to.equal('alert-input-component error');
+    });
+
+    it('should assign reference to root node', () => {
+      const component = mount(<AlertInput />);
+      const instance = component.instance();
+      const overlay = component.find(Overlay);
+      const targetFn = overlay.prop('target');
+      expect(instance.root).to.equal(targetFn());
+    });
   });
 });
