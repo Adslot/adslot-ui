e178f3dab219852eb39c18c8f767e2f28bde357a
Tuan Nguyen <lightbringer1991@yahoo.com>
2017-10-31 12:01:38 +1100

    Build: add development build into dist directory

--- config/dist.js
+++ config/dist.js
@@ -1,75 +1,91 @@
-// development config
+const _ = require('lodash');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const { resolve } = require('path');
 const commonConfig = require('./common');
 
-module.exports = merge(commonConfig, {
-  entry: {
+module.exports = () => {
+  const buildType = process.env.TYPE || 'development';
+
+  let entries = {
     main: resolve(__dirname, '../src/dist-entry'),
     core: resolve(__dirname, '../src/dist-entry/core'),
     extra: resolve(__dirname, '../src/dist-entry/extra'),
-  },
-  externals: {
-    lodash: {
-      root: '_',
-      commonjs2: 'lodash',
-      commonjs: 'lodash',
-      amd: 'lodash',
-    },
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-    },
-    'react-redux': {
-      root: 'reactRedux',
-      commonjs2: 'react-redux',
-      commonjs: 'react-redux',
-      amd: 'react-redux',
-    },
-    'redux': {
-      root: 'Redux',
-      commonjs2: 'redux',
-      commonjs: 'redux',
-      amd: 'redux',
-    },
-  },
-  output: {
-    path: resolve(__dirname, '../dist'),
-    filename: 'adslot-ui-[name].js',
-    libraryTarget: 'umd',
-    library: 'AdslotUI',
-  },
-  devtool: 'inline',
-  cache: false,
-  plugins: [
-    new webpack.DefinePlugin({ // ensures webpack will always optimise for production
-      'process.env.NODE_ENV': '"production"',
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      output: {
-        comments: false,
-      },
-      sourceMap: false,
-      include: /\.js$/,
-      parallel: {
-        cache: true,
-        workers: 2, // run on two cores #GSD
-      },
-    }),
+  };
+  const plugins = [
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
-});
+  ];
+
+  if (buildType === 'development') {
+    entries = _.mapKeys(entries, (value, key) => `${key}.dev`);
+  }
+
+  if (buildType === 'production') {
+    plugins.push(
+      new webpack.DefinePlugin({ // ensures webpack will always optimise for production
+        'process.env.NODE_ENV': '"production"',
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        },
+        output: {
+          comments: false,
+        },
+        sourceMap: false,
+        include: /\.js$/,
+        parallel: {
+          cache: true,
+          workers: 2, // run on two cores #GSD
+        },
+      })
+    );
+  }
+
+  return merge(commonConfig, {
+    entry: entries,
+    externals: {
+      lodash: {
+        root: '_',
+        commonjs2: 'lodash',
+        commonjs: 'lodash',
+        amd: 'lodash',
+      },
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+      'react-redux': {
+        root: 'reactRedux',
+        commonjs2: 'react-redux',
+        commonjs: 'react-redux',
+        amd: 'react-redux',
+      },
+      'redux': {
+        root: 'Redux',
+        commonjs2: 'redux',
+        commonjs: 'redux',
+        amd: 'redux',
+      },
+    },
+    output: {
+      path: resolve(__dirname, '../dist'),
+      filename: 'adslot-ui-[name].js',
+      libraryTarget: 'umd',
+      library: 'AdslotUI',
+    },
+    devtool: 'inline',
+    cache: false,
+    plugins,
+  });
+};
