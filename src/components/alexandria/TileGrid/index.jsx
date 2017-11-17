3254b4921ced8e119e439d457fe2bd10c68d189d
Xiaofan Wu <xiaofan2406@gmail.com>
2017-11-17 14:13:39 +1100

    Fix: fixing some proptypes definitions

--- src/components/alexandria/TileGrid/index.jsx
+++ src/components/alexandria/TileGrid/index.jsx
@@ -2,6 +2,7 @@
 import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
+import idPropType from '../../prop-types/idPropType';
 import './styles.scss';
 
 const TileGrid = ({ title, items, onItemClick }) => {
@@ -24,7 +25,7 @@ const TileGrid = ({ title, items, onItemClick }) => {
 TileGrid.propTypes = {
   title: PropTypes.string.isRequired,
   items: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    id: idPropType.isRequired,
     classSuffix: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
   })).isRequired,
