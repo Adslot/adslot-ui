cd1a0fd63734023c50ced76b411403d286f99ed4
Harry Nguyen <sonhanguyen@outlook.com>
2018-01-10 11:44:52 +1100

    Fix: import statement to point correctly to the lib version of react-datepicker

--- src/dist-entry/extra.js
+++ src/dist-entry/extra.js
@@ -1,4 +1,4 @@
 import 'styles/_react-datepicker-custom.scss';
-import DatePicker from 'react-datepicker/dist/react-datepicker';
+import DatePicker from 'react-datepicker';
 
 export { DatePicker }; // eslint-disable-line import/prefer-default-export
