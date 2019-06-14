de5b2bf3c8c55e9a29244da1f6a6ebf3875842c1
Vandish Gandhi <vandish.gandhi@adslot.com>
2019-06-14 14:36:19 +1000

    fix: add external to dev build

--- config/webpack.config.dev.build.js
+++ config/webpack.config.dev.build.js
@@ -12,13 +12,51 @@ module.exports = webpackMerge(commonConfig, {
   mode: 'development',
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
-  devtool: 'source-map',
+  devtool: 'cheap-eval-source-map',
   entry: paths.appSrc,
   output: {
     path: paths.appBuild,
     filename: 'adslot-ui.js',
     libraryTarget: 'umd',
     library: 'AdslotUI',
+  },
+  externals: {
+    lodash: {
+      root: '_',
+      commonjs2: 'lodash',
+      commonjs: 'lodash',
+      amd: 'lodash',
+    },
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+    'react-redux': {
+      root: 'reactRedux',
+      commonjs2: 'react-redux',
+      commonjs: 'react-redux',
+      amd: 'react-redux',
+    },
+    redux: {
+      root: 'Redux',
+      commonjs2: 'redux',
+      commonjs: 'redux',
+      amd: 'redux',
+    },
+    moment: {
+      root: 'moment',
+      commonjs2: 'moment',
+      commonjs: 'moment',
+      amd: 'moment',
+    },
   },
   module: {
     strictExportPresence: true,
