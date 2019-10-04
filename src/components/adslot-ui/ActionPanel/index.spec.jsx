e31d1627993af245c0dd2eab93ac3be09f712a42
Namrata Trivedi <namrata.trivedi@adslot.com>
2019-10-04 14:59:29 +1000

    test: action panel button test

--- src/components/adslot-ui/ActionPanel/index.spec.jsx
+++ src/components/adslot-ui/ActionPanel/index.spec.jsx
@@ -22,6 +22,11 @@ describe('ActionPanelComponent', () => {
     expect(headerElement).to.have.length(1);
     expect(headerElement.find('.title').text()).to.equal('Action Panel');
 
+    const closeButton = wrapper.find('.close-button');
+    expect(closeButton.children().text()).to.equal('');
+    const closeIcon = closeButton.find('.close-icon');
+    expect(closeIcon).to.have.length(1);
+
     const bodyElement = wrapper.find('.aui--action-panel-body');
     expect(bodyElement).to.have.length(1);
 
@@ -29,16 +34,36 @@ describe('ActionPanelComponent', () => {
     expect(document.body.classList.contains('modal-open')).to.equal(false);
   });
 
-  it('should render as a modal', () => {
-    const wrapper = shallow(
-      <ActionPanel {...makeProps({ isModal: true, size: 'large', actionButton: <Button>Action</Button> })} />
-    );
-
+  it('should render as a modal with close icon', () => {
+    const wrapper = shallow(<ActionPanel {...makeProps({ isModal: true, size: 'large' })} />);
     expect(document.body.classList.contains('modal-open')).to.equal(true);
 
     expect(wrapper.find('.aui--action-panel-modal-wrapper')).to.have.length(1);
     const actionPanelElement = wrapper.find('.aui--action-panel');
     expect(actionPanelElement.prop('className')).to.equal('aui--action-panel is-large action-modal');
+
+    const closeButton = wrapper.find('.close-button');
+    expect(closeButton.children().text()).to.equal('');
+    const closeIcon = wrapper.find('.close-button .close-icon');
+    expect(closeIcon).to.have.length(1);
+
+    wrapper.instance().componentWillUnmount();
+    expect(document.body.classList.contains('modal-open')).to.equal(false);
+  });
+
+  it('should render as a modal with cancel and action buttons', () => {
+    const wrapper = shallow(
+      <ActionPanel
+        {...makeProps({ isModal: true, size: 'large', actionButton: <Button dts="action-button">Action</Button> })}
+      />
+    );
+    expect(document.body.classList.contains('modal-open')).to.equal(true);
+    const closeButton = wrapper.find('.close-button');
+    expect(closeButton.children().text()).to.equal('Cancel');
+    const closeIcon = closeButton.find('.close-icon');
+    expect(closeIcon).to.have.length(0);
+    const actionButton = wrapper.find("[dts='action-button']");
+    expect(actionButton).to.have.length(1);
 
     wrapper.instance().componentWillUnmount();
     expect(document.body.classList.contains('modal-open')).to.equal(false);
