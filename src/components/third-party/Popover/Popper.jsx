cc28d56e2f292255b965e47c0ac3c1b62dfd9cda
Tuan Nguyen <lightbringer1991@yahoo.com>
2019-11-27 11:55:24 +1100

    fix: fix popover with ref showing incorrect arrow position

--- src/components/third-party/Popover/Popper.jsx
+++ src/components/third-party/Popover/Popper.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Popper as ReactPopper } from 'react-popper';
 import { popoverPlacements } from './constants';
+
+// default arrow position in pixel from the corresponding popover edge
+const DEFAULT_ARROW_POSITION = 12;
+
+/**
+ *
+ * @param {string} placement
+ * @param {object} arrowStyles            user-defined arrow styles
+ * @param {Element} container             anchor container
+ */
+export const renderArrowStyles = (placement, arrowStyles, container = null) => {
+  const horizontalPosition =
+    _.get(container, 'clientWidth') >= DEFAULT_ARROW_POSITION * 2 ? DEFAULT_ARROW_POSITION : null;
+  const verticalPosition =
+    _.get(container, 'clientHeight') >= DEFAULT_ARROW_POSITION * 2 ? DEFAULT_ARROW_POSITION : null;
+
+  let calculatedArrowStyles = {};
+  switch (true) {
+    case _.includes(['bottom-start', 'top-start'], placement) && !_.isNull(horizontalPosition):
+      calculatedArrowStyles = { left: horizontalPosition };
+      break;
+    case _.includes(['bottom-end', 'top-end'], placement) && !_.isNull(horizontalPosition):
+      calculatedArrowStyles = { left: 'auto', right: horizontalPosition };
+      break;
+    case _.includes(['left-start', 'right-start'], placement) && !_.isNull(verticalPosition):
+      calculatedArrowStyles = { top: verticalPosition };
+      break;
+    case _.includes(['left-end', 'right-end'], placement) && !_.isNull(verticalPosition):
+      calculatedArrowStyles = { top: 'auto', bottom: verticalPosition };
+      break;
+    default:
+      calculatedArrowStyles = {};
+  }
+
+  return {
+    ...calculatedArrowStyles,
+    ...arrowStyles, // let user override default configuration
+  };
+};
 
 const Popper = ({
   arrowStyles,
@@ -16,43 +55,47 @@ const Popper = ({
   refElement,
   title,
   wrapperStyles,
-}) => (
-  <ReactPopper
-    {...(refElement ? { referenceElement: refElement } : {})}
-    innerRef={innerRef}
-    placement={popperPlacement}
-    modifiers={{
-      preventOverflow: {
-        enabled: true,
-        boundariesElement,
-      },
-      ...modifiers,
-    }}
-  >
-    {({ ref, style, placement, arrowProps, scheduleUpdate }) => (
-      <div
-        className={popoverClass}
-        ref={ref}
-        style={{ ...style, ...wrapperStyles }}
-        data-placement={placement}
-        data-test-selector={dts}
-      >
-        <div className="aui--popover-container">
-          {title ? <div className="popover-title">{title}</div> : null}
-          <div className="popover-content">
-            {_.isFunction(popoverContent) ? popoverContent({ scheduleUpdate }) : popoverContent}
-          </div>
-        </div>
+}) => {
+  const calculatedArrowStyles = renderArrowStyles(popperPlacement, arrowStyles, refElement);
+
+  return (
+    <ReactPopper
+      {...(refElement ? { referenceElement: refElement } : {})}
+      innerRef={innerRef}
+      placement={popperPlacement}
+      modifiers={{
+        preventOverflow: {
+          enabled: true,
+          boundariesElement,
+        },
+        ...modifiers,
+      }}
+    >
+      {({ ref, style, placement, arrowProps, scheduleUpdate }) => (
         <div
-          className="aui--popover-arrow"
+          className={popoverClass}
+          ref={ref}
+          style={{ ...style, ...wrapperStyles }}
           data-placement={placement}
-          ref={arrowProps.ref}
-          style={{ ...arrowProps.style, ...arrowStyles }}
-        />
-      </div>
-    )}
-  </ReactPopper>
-);
+          data-test-selector={dts}
+        >
+          <div className="aui--popover-container">
+            {title ? <div className="popover-title">{title}</div> : null}
+            <div className="popover-content">
+              {_.isFunction(popoverContent) ? popoverContent({ scheduleUpdate }) : popoverContent}
+            </div>
+          </div>
+          <div
+            className="aui--popover-arrow"
+            data-placement={placement}
+            ref={arrowProps.ref}
+            style={{ ...arrowProps.style, ...calculatedArrowStyles }}
+          />
+        </div>
+      )}
+    </ReactPopper>
+  );
+};
 
 Popper.propTypes = {
   arrowStyles: PropTypes.object, // eslint-disable-line react/forbid-prop-types
