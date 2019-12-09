9f933377f66379a8daa2a79dbed8c45cd61297c2
Glen Kurniawan <glen.kurniawan@adslot.com>
2019-12-09 16:33:29 +1100

    fix: fix test3

--- src/components/DatePicker/index.spec.jsx
+++ src/components/DatePicker/index.spec.jsx
@@ -5,11 +5,11 @@ import DatePicker from '.';
 describe('DatePicker', () => {
   it('should render with defaults', () => {
     const component = shallow(<DatePicker className="test" />);
-    expect(component.props().handleDateChangeRaw).toBeUndefined();
+    expect(component.props().handleDateChangeRaw).to.be.undefined;
   });
 
   it('should render with onChangeRaw when disableInlineEditing = true', () => {
     const component = shallow(<DatePicker className="test" disableInlineEditing={true} />);
-    expect(component.props().hasOwnProperty('handleDateChangeRaw')).toBe(true);
+    expect(component.props().hasOwnProperty('handleDateChangeRaw')).to.be(true);
   });
 });
