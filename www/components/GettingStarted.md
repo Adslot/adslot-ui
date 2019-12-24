53bfa5d5b7582fcd0e7bb2b51a181e2aa5c747ca
Vandish Gandhi <mastermindvdg@gmail.com>
2019-12-24 14:10:18 +1100

    chore: setup mdx infra

---
+++ www/components/GettingStarted.md
@@ -0,0 +1,9 @@
+## Getting Started
+
+### Installation
+
+Adslot-ui is available as an npm package.
+
+```
+npm install adslot-ui
+```
---
+++ www/containers/CodeBlock.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import { mdx } from '@mdx-js/react';
+import * as AdslotUI from '../../src';
+import theme from './theme';
+
+/* eslint-disable */
+export default ({ children, className, live }) => {
+  if (live) {
+    return (
+      <LiveProvider
+        code={children.trim()}
+        language="jsx"
+        noInline={children.includes('render(')}
+        scope={{ ...AdslotUI, mdx }}
+        theme={theme}
+      >
+        <LivePreview style={{ marginTop: '20px' }} />
+        <LiveError className="aui--docs-code-error" />
+        <h4 style={{ marginTop: '40px' }}>Try editing the livecode below:</h4>
+        <LiveEditor />
+      </LiveProvider>
+    );
+  }
+  return (
+    <Highlight {...defaultProps} code={children} language="javascript" theme={theme}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, paddingTop: '15px', paddingLeft: '15px', minWidth: '70vw' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+/* eslint-enable */
