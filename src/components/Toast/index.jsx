08301eda94bd70df6f141c2d6e7800e0a7295222
Yonghui Wang <yonghui.wang@adslot.com>
2020-07-17 12:20:41 +1000

    fix: toast invalid hook issue

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -1,8 +1,10 @@
 import ToastContainerComponent from './ToastContainer';
 import ToastNotificationComponent from './ToastNotification';
+import { notify } from './ToastNotification';
 
 const Toast = {};
 Toast.Container = ToastContainerComponent;
 Toast.Notification = ToastNotificationComponent;
+Toast.notify = notify;
 
 export default Toast;
