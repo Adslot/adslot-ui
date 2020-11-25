333c1759260a0d6a5b5dc10290f153502eb89b5b
knilink <esurllink@gmail.com>
2020-11-25 17:41:27 +1100

    refactor: upgrade react-datepicker

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -2,29 +2,79 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDatePicker from 'react-datepicker';
+import moment from 'moment';
 import './styles.scss';
 
 const adslotDatePickerPropTypes = {
   disableInlineEditing: PropTypes.bool,
 };
 
-class DatePicker extends React.PureComponent {
-  handleDateChangeRaw = event => {
-    event.preventDefault();
-  };
+const momentToDate = date => (!date || date instanceof Date ? date : date.toDate());
 
-  render() {
-    const { disableInlineEditing } = this.props;
+const withMoment = DatePickerComponent =>
+  React.forwardRef(({ selected, onChange, startDate, endDate, minDate, maxDate, ...props }, ref) => {
+    const isDate = selected instanceof Date;
+    const handleChange = React.useCallback(
+      newDate => {
+        onChange && onChange(!isDate && newDate ? moment(newDate) : newDate);
+      },
+      [isDate, onChange]
+    );
+    return (
+      <DatePickerComponent
+        ref={ref}
+        {...props}
+        selected={momentToDate(selected)}
+        startDate={momentToDate(startDate)}
+        endDate={momentToDate(endDate)}
+        minDate={momentToDate(minDate)}
+        maxDate={momentToDate(maxDate)}
+        onChange={handleChange}
+      />
+    );
+  });
 
-    const datePickerProps = disableInlineEditing ? { onChangeRaw: this.handleDateChangeRaw } : {};
+const RefHack = React.forwardRef(({ originalCustomInputRef, originalCustomInput, _datePickerRef, ...props }, ref) => {
+  const _ref = React.useRef(null);
+  React.useLayoutEffect(() => {
+    if (typeof ref === 'function') ref(_ref.current);
+    else if (typeof ref === 'object') ref.current = _ref.current;
+    _datePickerRef(_ref.current);
+    return () => {
+      if (typeof ref === 'function') ref(_ref.current);
+      else if (typeof ref === 'object') ref.current = null;
+      _datePickerRef(null);
+    };
+  }, [_datePickerRef, ref, _ref]);
+  return React.cloneElement(originalCustomInput, { ...props, [originalCustomInputRef]: _ref });
+});
+
+const DatePicker = withMoment(
+  React.forwardRef(({ dts, disableInlineEditing, customInputRef, customInput, ...props }, ref) => {
+    const handleDateChangeRaw = React.useCallback(event => {
+      event.preventDefault();
+    }, []);
+
+    const datePickerProps = disableInlineEditing ? { onChangeRaw: handleDateChangeRaw } : {};
 
     return (
-      <div data-testid="date-picker-wrapper" className="aui--date-picker" data-test-selector={this.props.dts}>
-        <ReactDatePicker {..._.omit(this.props, _.keys(adslotDatePickerPropTypes))} {...datePickerProps} />
+      <div data-testid="date-picker-wrapper" className="aui--date-picker" data-test-selector={dts}>
+        <ReactDatePicker
+          {..._.omit(props, _.keys(adslotDatePickerPropTypes))}
+          {...datePickerProps}
+          customInputRef="_datePickerRef"
+          customInput={
+            <RefHack
+              ref={ref}
+              originalCustomInputRef={customInputRef || 'ref'}
+              originalCustomInput={customInput || <input type="text" />}
+            />
+          }
+        />
       </div>
     );
-  }
-}
+  })
+);
 
 DatePicker.propTypes = { ...adslotDatePickerPropTypes };
 
