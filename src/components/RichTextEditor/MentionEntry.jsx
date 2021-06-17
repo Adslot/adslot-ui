97fac2c7e90acdf643b3c353b2138578f63e0976
Yonghui Wang <harris.wang@mail.com>
2021-06-17 13:47:39 +1000

    feat: rich text editor with mention

---
+++ src/components/RichTextEditor/MentionEntry.jsx
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import classnames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '../Avatar';
+
+const MentionEntry = (
+  { mention: { name, title, avatar }, ...props } // internal props used by mention plugin
+) => (
+  <div
+    className={classnames(['aui--mention-entry', { 'aui--mention-entry__is-focused': props.isFocused }])}
+    data-testid="rich-text-editor-mention-entry"
+    {..._.pick(props, ['onMouseDown', 'onMouseUp', 'onMouseEnter'])}
+  >
+    <div className="mention-entry--container">
+      <div className="mention-entry--container__left">
+        <Avatar image={avatar} />
+      </div>
+
+      <div className="mention-entry--container__right">
+        <div className="mention-entry--name">{name}</div>
+        <div className="mention-entry--title">{title}</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default MentionEntry;
+
+MentionEntry.propTypes = {
+  className: PropTypes.string,
+  mention: PropTypes.shape({
+    name: PropTypes.string,
+    title: PropTypes.string,
+    avatar: PropTypes.string,
+  }),
+  onMouseDown: PropTypes.func,
+  onMouseUp: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  isFocused: PropTypes.bool,
+};
