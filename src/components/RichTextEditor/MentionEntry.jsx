c0c933a842a31b36dd37ca4bb5b405c8467214a1
Yonghui Wang <harris.wang@mail.com>
2021-06-17 13:47:39 +1000

    feat: rich text editor with file upload

--- src/components/RichTextEditor/MentionEntry.jsx
+++ src/components/RichTextEditor/MentionEntry.jsx
@@ -6,24 +6,30 @@ import Avatar from '../Avatar';
 
 const MentionEntry = (
   { mention: { name, title, avatar }, ...props } // internal props used by mention plugin
-) => (
-  <div
-    className={classnames(['aui--mention-entry', { 'aui--mention-entry__is-focused': props.isFocused }])}
-    data-testid="rich-text-editor-mention-entry"
-    {..._.pick(props, ['onMouseDown', 'onMouseUp', 'onMouseEnter'])}
-  >
-    <div className="mention-entry--container">
-      <div className="mention-entry--container__left">
-        <Avatar image={avatar} />
-      </div>
+) => {
+  const fullName = name.split(' ');
+  const givenName = fullName[0];
+  const surname = fullName.length > 1 ? fullName[fullName.length - 1] : '';
 
-      <div className="mention-entry--container__right">
-        <div className="mention-entry--name">{name}</div>
-        <div className="mention-entry--title">{title}</div>
+  return (
+    <div
+      className={classnames(['aui--mention-entry', { 'aui--mention-entry__is-focused': props.isFocused }])}
+      data-testid="rich-text-editor-mention-entry"
+      {..._.pick(props, ['onMouseDown', 'onMouseUp', 'onMouseEnter'])}
+    >
+      <div className="mention-entry--container">
+        <div className="mention-entry--container__left">
+          <Avatar givenName={givenName} surname={surname} image={avatar} />
+        </div>
+
+        <div className="mention-entry--container__right">
+          <div className="mention-entry--name">{name}</div>
+          <div className="mention-entry--title">{title}</div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MentionEntry;
 
