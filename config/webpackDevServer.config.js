5b0a9b6760cd19cd21862e1ab487e94dd6ef7e7d
knilink <esurllink@gmail.com>
2021-08-25 15:28:49 +1000

    chore: upgrade webpack dev server

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -7,48 +7,33 @@ const host = process.env.HOST || '0.0.0.0';
 // eslint-disable-next-line
 module.exports = function (proxy, allowedHost) {
   return {
-    // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
-    // websites from potentially accessing local content through DNS rebinding:
-    // https://github.com/webpack/webpack-dev-server/issues/887
-    // https://medium.com/webpack/webpack-dev-server-middleware-security-issues-1489d950874a
-    // However, it made several existing use cases such as development in cloud
-    // environment or subdomains in development significantly more complicated:
-    // https://github.com/facebookincubator/create-react-app/issues/2271
-    // https://github.com/facebookincubator/create-react-app/issues/2233
-    // While we're investigating better solutions, for now we will take a
-    // compromise. Since our WDS configuration only serves files in the `public`
-    // folder we won't consider accessing them a vulnerability. However, if you
-    // use the `proxy` feature, it gets more dangerous because it can expose
-    // remote code execution vulnerabilities in backends like Django and Rails.
-    // So we will disable the host check normally, but enable it if you have
-    // specified the `proxy` setting. Finally, we let you override it if you
-    // really know what you're doing with a special environment variable.
-    disableHostCheck: !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     // Enable gzip compression of generated files.
     compress: true,
     // Silence WebpackDevServer's own logs since they're generally not useful.
     // It will still show compile warnings and errors with this setting.
-    // clientLogLevel: 'none',
-    clientLogLevel: 'info',
-    contentBase: paths.appPublic,
-    // By default files from `contentBase` will not trigger a page reload.
-    watchContentBase: true,
+    client: {
+      logging: 'none',
+    },
+    static: {
+      directory: paths.assetsPath,
+      publicPath: '/assets',
+      watch: true,
+    },
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
     // for the WebpackDevServer client so it can learn when the files were
     // updated. The WebpackDevServer client is included as an entry point
     // in the Webpack development configuration. Note that only changes
     // to CSS are currently hot reloaded. JS changes will refresh the browser.
     hot: true,
-    // It is important to tell WebpackDevServer to use the same "root" path
-    // as we specified in the config. In development, we always serve from /.
-    publicPath: config.output.publicPath,
-    // WebpackDevServer is noisy by default so we emit custom message instead
-    // by listening to the compiler events with `compiler.plugin` calls above.
-    quiet: true,
     // Reportedly, this avoids CPU overload on some systems.
     // https://github.com/facebookincubator/create-react-app/issues/293
-    watchOptions: {
-      ignored: /node_modules|__test__|__snapshots__/,
+    watchFiles: {
+      // advance chokidar options to exclude
+      // https://webpack.js.org/configuration/dev-server/#devserverwatchfiles
+      // https://github.com/paulmillr/chokidar
+      options: {
+        ignored: /node_modules|__test__|__snapshots__/,
+      },
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
@@ -58,7 +43,12 @@ module.exports = function (proxy, allowedHost) {
       // See https://github.com/facebookincubator/create-react-app/issues/387.
       disableDotRule: true,
     },
-    public: allowedHost,
+    allowedHosts: process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true' ? 'all' : [allowedHost],
     proxy,
+    devMiddleware: {
+      // It is important to tell WebpackDevServer to use the same "root" path
+      // as we specified in the config. In development, we always serve from /.
+      publicPath: config.output.publicPath,
+    },
   };
 };
