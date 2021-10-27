8a0055120bde58066ff80cf7fb0847ea08685318
chaofan <chaofan1801@gmail.com>
2021-10-28 10:56:19 +1100

    refactor: convert Accordion to functional component

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -4,79 +4,29 @@ import PropTypes from 'prop-types';
 import Card from '../Card';
 import Panel from '../Panel';
 
-class Accordion extends React.PureComponent {
-  static propTypes = {
-    /**
-     * render `data-test-selector` onto the component. It can be useful for testing.
-     */
-    dts: PropTypes.string,
-    /**
-     * onPanelClick(panelId) takes in a single parameter which is the id of the clicked panel.
-     */
-    onPanelClick: PropTypes.func,
-    /**
-     * <span>
-     *  Accept an array of <a href="/panel-example">Panel</a> or
-     *  <a href="/accordion-panel-example">Accordion.Panel</a>
-     *  </span>
-     */
-    children: PropTypes.node,
-    defaultActivePanelIds: PropTypes.arrayOf(PropTypes.string),
-    /**
-     * Determine how many Panels can be expanded, accepted value is a positive number, or <code>max</code> to have no restriction
-     */
-    maxExpand: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['max'])]),
-  };
+const Accordion = ({ dts, children, maxExpand, defaultActivePanelIds, onPanelClick }) => {
+  const [activePanelIds, setActivePanelIds] = React.useState(() => {
+    return maxExpand === 'max' ? defaultActivePanelIds : _.slice(defaultActivePanelIds, 0, maxExpand);
+  });
 
-  static defaultProps = {
-    maxExpand: 'max',
-    defaultActivePanelIds: [],
-  };
-
-  state = {
-    activePanelIds:
-      this.props.maxExpand === 'max'
-        ? this.props.defaultActivePanelIds
-        : _.slice(this.props.defaultActivePanelIds, 0, this.props.maxExpand),
-  };
-
-  onPanelClick = (panelId) => {
-    const { maxExpand } = this.props;
-
-    if (_.includes(this.state.activePanelIds, panelId)) {
+  const onChildPanelClick = (panelId) => {
+    if (_.includes(activePanelIds, panelId)) {
       // remove panelId out of the active list
-      this.setState((prevState) => ({ activePanelIds: _.without(prevState.activePanelIds, panelId) }));
+      setActivePanelIds((prevState) => _.without(prevState, panelId));
     } else {
       // drop panels from the beginning if max opened panels count is reached
-      this.setState((prevState) => {
-        let newActivePanelIds = [...prevState.activePanelIds, panelId];
+      setActivePanelIds((prevState) => {
+        let newActivePanelIds = [...prevState, panelId];
         if (maxExpand !== 'max' && newActivePanelIds.length > maxExpand) {
           newActivePanelIds = _.drop(newActivePanelIds, newActivePanelIds.length - maxExpand);
         }
-
-        return { activePanelIds: newActivePanelIds };
+        return newActivePanelIds;
       });
     }
-
-    if (this.props.onPanelClick) {
-      this.props.onPanelClick(panelId);
-    }
+    onPanelClick?.(panelId);
   };
 
-  validateProps() {
-    const { maxExpand } = this.props;
-
-    // validate maxExpand value
-    switch (true) {
-      case _.isNumber(maxExpand) && maxExpand <= 0:
-      case _.isString(maxExpand) && maxExpand !== 'max':
-        throw new Error("maxExpand must be a positive number or 'max'");
-      default:
-        break;
-    }
-  }
-
-  renderPanelFromChildren = (child) => {
+  const renderPanelFromChildren = (child) => {
     const { id, isCollapsed } = child.props;
 
     // prevent rendering if child is not an instance of Accordion.Panel
@@ -87,23 +37,48 @@ class Accordion extends React.PureComponent {
     // respects child.props.isCollapsed for controlled behavior
     return React.cloneElement(child, {
       ...child.props,
-      onClick: this.onPanelClick,
-      isCollapsed: _.isNil(isCollapsed) ? !_.includes(this.state.activePanelIds, id) : isCollapsed,
+      onClick: onChildPanelClick,
+      isCollapsed: _.isNil(isCollapsed) ? !_.includes(activePanelIds, id) : isCollapsed,
     });
   };
 
-  render() {
-    const { children, dts } = this.props;
-    this.validateProps();
+  if ((_.isNumber(maxExpand) && maxExpand <= 0) || (_.isString(maxExpand) && maxExpand !== 'max'))
+    throw new Error("maxExpand must be a positive number or 'max'");
 
-    return (
-      <Card.Container dts={dts}>
-        <Card.Content fill>{React.Children.map(children, this.renderPanelFromChildren)}</Card.Content>
-      </Card.Container>
-    );
-  }
-}
+  return (
+    <Card.Container dts={dts}>
+      <Card.Content fill>{React.Children.map(children, renderPanelFromChildren)}</Card.Content>
+    </Card.Container>
+  );
+};
+
+Accordion.prototypes = {
+  /**
+   * render `data-test-selector` onto the component. It can be useful for testing.
+   */
+  dts: PropTypes.string,
+  /**
+   * onPanelClick(panelId) takes in a single parameter which is the id of the clicked panel.
+   */
+  onPanelClick: PropTypes.func,
+  /**
+   * <span>
+   *  Accept an array of <a href="/panel-example">Panel</a> or
+   *  <a href="/accordion-panel-example">Accordion.Panel</a>
+   *  </span>
+   */
+  children: PropTypes.node,
+  defaultActivePanelIds: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Determine how many Panels can be expanded, accepted value is a positive number, or <code>max</code> to have no restriction
+   */
+  maxExpand: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['max'])]),
+};
+
+Accordion.defaultProps = {
+  maxExpand: 'max',
+  defaultActivePanelIds: [],
+};
 
 Accordion.Panel = Panel;
-
 export default Accordion;
