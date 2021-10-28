9fef8ead397a8ab68f7c04bd8decd2f6c330c7c8
chaofan <chaofan1801@gmail.com>
2021-10-28 11:42:20 +1100

    refactor: convert FilePicker into functional component

--- src/components/FilePicker/index.jsx
+++ src/components/FilePicker/index.jsx
@@ -6,122 +6,110 @@ import './styles.scss';
 
 const baseClass = 'filepicker-component';
 
-class FilePickerComponent extends React.PureComponent {
-  static propTypes = {
-    /**
-     * determines if the filePicker is disabled
-     */
-    disabled: PropTypes.bool,
-    /**
-     * data-test-selector of the filePicker
-     */
-    dts: PropTypes.string,
-    /**
-     * determines what file types the user can pick from the file input dialog box
-     */
-    filter: PropTypes.string,
-    /**
-     * determines if the filePicker is highlighted or not
-     */
-    isHighlighted: PropTypes.bool,
-    /**
-     * the label to be displayed
-     */
-    label: PropTypes.string,
-    /**
-     * function called when onRemove event is fired
-     */
-    onRemove: PropTypes.func,
-    /**
-     * function called when onSelect event is fired
-     */
-    onSelect: PropTypes.func.isRequired,
-    /**
-     * determines the placeholder when no date is selected
-     */
-    placeholder: PropTypes.string,
-  };
+const FilePicker = ({ filter, dts, placeholder, onSelect, onRemove, isHighlighted, disabled, label }) => {
+  const fileInputRef = React.useRef();
+  const [fileName, setFileName] = React.useState('');
+  const isFileSelected = !!fileName;
 
-  static defaultProps = {
-    isHighlighted: false,
-    label: 'Select',
-    placeholder: 'No file selected',
-    disabled: false,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.fileInput = React.createRef();
-  }
-
-  state = {
-    isFileSelected: false,
-    fileName: '',
-  };
-
-  onChange = (changeEvent) => {
-    if (!this.state.isFileSelected) {
-      this.setState({ isFileSelected: true, fileName: changeEvent.target.files[0].name });
-      this.props.onSelect(changeEvent.target.files[0]);
+  const onChange = (event) => {
+    if (!isFileSelected) {
+      setFileName(event.target.files[0].name);
+      onSelect(event.target.files[0]);
     }
   };
 
-  onUploadBtnClick = () => {
-    this.fileInput.current.click();
+  const onUploadBtnClick = () => {
+    fileInputRef.current.click();
   };
 
-  removeFile = () => {
-    this.fileInput.current.value = null;
-    this.setState({ isFileSelected: false, fileName: '' });
-    if (this.props.onRemove) {
-      this.props.onRemove();
-    }
+  const removeFile = () => {
+    fileInputRef.current.value = null;
+    setFileName('');
+    onRemove?.();
   };
 
-  render() {
-    const mainClass = classNames({ [`${baseClass}-highlight`]: this.props.isHighlighted }, baseClass, 'input-group');
-    const { isFileSelected, fileName } = this.state;
+  const mainClass = classNames({ [`${baseClass}-highlight`]: isHighlighted }, baseClass, 'input-group');
 
-    return (
-      <div data-testid="file-picker-wrapper" className={mainClass}>
-        <input
-          data-testid="file-picker-form-control"
-          className="form-control"
-          type="text"
-          disabled
-          placeholder={this.props.placeholder}
-          readOnly="readonly"
-          value={fileName}
-          title={fileName}
-        />
-        <div className="input-group-btn">
-          {isFileSelected ? (
-            <Button data-testid="file-picker-remove-button" className="remove-file" onClick={this.removeFile}>
-              ×
-            </Button>
-          ) : null}
-          <Button
-            data-testid="file-picker-input-button"
-            inverse
-            onClick={this.onUploadBtnClick}
-            disabled={this.props.disabled || isFileSelected}
-          >
-            <span data-testid="file-picker-input-button-label">{this.props.label}</span>
-            <input
-              data-testid="file-picker-input-button-input"
-              className="file-input"
-              ref={this.fileInput}
-              type="file"
-              onChange={this.onChange}
-              accept={this.props.filter}
-              data-test-selector={this.props.dts}
-            />
+  return (
+    <div data-testid="file-picker-wrapper" className={mainClass}>
+      <input
+        data-testid="file-picker-form-control"
+        className="form-control"
+        type="text"
+        disabled
+        placeholder={placeholder}
+        readOnly="readonly"
+        value={fileName}
+        title={fileName}
+      />
+      <div className="input-group-btn">
+        {isFileSelected ? (
+          <Button data-testid="file-picker-remove-button" className="remove-file" onClick={removeFile}>
+            ×
           </Button>
-        </div>
+        ) : null}
+        <Button
+          data-testid="file-picker-input-button"
+          inverse
+          onClick={onUploadBtnClick}
+          disabled={disabled || isFileSelected}
+        >
+          <span data-testid="file-picker-input-button-label">{label}</span>
+          <input
+            data-testid="file-picker-input-button-input"
+            className="file-input"
+            ref={fileInputRef}
+            type="file"
+            onChange={onChange}
+            accept={filter}
+            data-test-selector={dts}
+          />
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default FilePickerComponent;
+FilePicker.propTypes = {
+  /**
+   * determines if the filePicker is disabled
+   */
+  disabled: PropTypes.bool,
+  /**
+   * data-test-selector of the filePicker
+   */
+  dts: PropTypes.string,
+  /**
+   * determines what file types the user can pick from the file input dialog box
+   */
+  filter: PropTypes.string,
+  /**
+   * determines if the filePicker is highlighted or not
+   */
+  isHighlighted: PropTypes.bool,
+  /**
+   * the label to be displayed
+   */
+  label: PropTypes.string,
+  /**
+   * function called when onRemove event is fired
+   */
+  onRemove: PropTypes.func,
+  /**
+   * function called when onSelect event is fired
+   */
+  onSelect: PropTypes.func.isRequired,
+  /**
+   * determines the placeholder when no date is selected
+   */
+  placeholder: PropTypes.string,
+};
+
+FilePicker.defaultProps = {
+  isHighlighted: false,
+  label: 'Select',
+  placeholder: 'No file selected',
+  disabled: false,
+};
+
+export default FilePicker;
