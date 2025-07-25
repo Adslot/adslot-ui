352f49395801e0fb9aed6c62b71f80a75f562d0e
Xiaofan Wu <xiaofan2406@gmail.com>
2025-07-25 12:06:21 +1000

    chore: v9

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import Toast from './index';
 import Button from '../Button';
