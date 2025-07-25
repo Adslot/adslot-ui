482d9bca8daa703fc0742d88beb8a0f24e93ef86
Benny Chew <benny.chew@adslot.com>
2025-07-25 10:16:18 +1000

    chore: resolve code scan alert

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const paths = require('./paths');
 
+const nodePathArray = (process.env.NODE_PATH || '').split(path.delimiter).filter(Boolean);
+process.env.NODE_PATH = nodePathArray;
+
 module.exports = {
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    modules: ['node_modules', paths.appNodeModules].concat(
-      (process.env.NODE_PATH = (process.env.NODE_PATH || '').split(path.delimiter).filter(Boolean))
-    ),
+    modules: ['node_modules', paths.appNodeModules].concat(nodePathArray),
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
